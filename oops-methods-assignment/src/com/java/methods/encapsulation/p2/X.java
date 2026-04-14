e2cb1838f0c4dfb6a60ea2f89d2dfe4dee3588a1
Sandeeptumu <tumusandeep0000@gmail.com>
2026-04-14 11:25:31 +0530

    encapsulation

---
+++ oops-methods-assignment/src/com/java/methods/encapsulation/p2/X.java
@@ -0,0 +1,10 @@
+package com.java.methods.encapsulation.p2;
+
+import com.java.methods.encapsulation.p1.A;
+
+public class X {
+    public static void main(String[] args) {
+        A a;
+        a = new A();
+    }
+}
---
+++ oops-methods-assignment/src/com/java/methods/encapsulation/p2/Y.java
@@ -0,0 +1,11 @@
+package com.java.methods.encapsulation.p2;
+
+import com.java.methods.encapsulation.p1.A;
+
+public class Y extends A {
+
+    public static void main(String[] args) {
+        Y y = new Y();
+        System.out.println(y.value);
+    }
+}
